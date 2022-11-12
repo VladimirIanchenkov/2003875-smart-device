980c635f27ebdca5a454889b3a914a82dd3b08ce
VladimirIanchenkov <ianchenkov.v@gmail.com>
2022-11-12 19:47:45 +0700

    внес исправления согласно замечаниям

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -1,9 +1,11 @@
+const PADDING = 20;
 const accordions = document.querySelectorAll('.main-footer__accordion');
 
 const initAccordion = () => {
   Array.prototype.slice.call(accordions).forEach((el) => {
     el.classList.remove('main-footer__accordion--nojs');
     el.classList.remove('main-footer__accordion--active');
+    el.nextElementSibling.style.paddingBottom = 0;
   });
 };
 
@@ -13,13 +15,16 @@ const toggleAccordion = () => {
       const panel = accordions[i].nextElementSibling;
       if (panel.style.maxHeight) {
         accordions[i].classList.remove('main-footer__accordion--active');
+        panel.style.paddingBottom = 0;
         panel.style.maxHeight = null;
       } else {
         Array.prototype.slice.call(accordions).forEach((el) => {
           el.classList.remove('main-footer__accordion--active');
+          el.nextElementSibling.style.paddingBottom = 0;
           el.nextElementSibling.style.maxHeight = null;
           accordions[i].classList.add('main-footer__accordion--active');
         });
+        panel.style.paddingBottom = PADDING + 'px';
         panel.style.maxHeight = panel.scrollHeight + 'px';
       }
     });
